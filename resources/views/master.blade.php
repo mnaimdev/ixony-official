f77430b075c0788fed02eb5f34f110e27533be90
mnaimdev <mnaimdev@gmail.com>
2023-10-11 05:04:56 -0700

    added some design

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css" integrity="sha512-yHknP1/AwR+yx26cB1y0cjvQUMvEa2PFzt1c9LlS4pRQ5NOTZFWbhBig+X9G9eYW/8m0/4OXNx8pxJ6z57x0dw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css"/>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
@@ -71,17 +72,39 @@
 
    {{-- slider --}}
 
-    <div class="slider row">
-            <div class="col-lg-12">
-
+        <div class="slider">
+            <div class="d-flex justify-content-center">
+                <div class="slider-info">
+                    <p>Mask</p>
+                    <p>Industrial PPE</p>
+                </div>
+                <div>
+                    <img src="{{asset('/slider1.png')}}" style="width: 100%; height: 300px;" alt="">
+                </div>
             </div>
-            <div class="col-lg-12">
 
-            </div>
-            <div class="col-lg-12">
 
+            <div class="d-flex justify-content-center">
+                <div class="slider-info">
+                    <p>Mask</p>
+                    <p>Industrial PPE</p>
+                </div>
+                <div>
+                    <img src="{{asset('/slider2.png')}}" style="width: 100%; height: 300px;" alt="">
+                </div>
             </div>
-  </div>
+
+
+            <div class="d-flex justify-content-center">
+                <div class="slider-info">
+                    <p>Mask</p>
+                    <p>Industrial PPE</p>
+                </div>
+                <div>
+                    <img src="{{asset('/slider3.png')}}" style="width: 100%; height: 300px;" alt="">
+                </div>
+            </div>
+          </div>
 
 {{--
     <div class="text-center mt-4 showcase p-5">
@@ -92,7 +115,7 @@
         <button class="bg-danger">Contact Us</button>
     </div> --}}
 
-
+{{-- shipping --}}
     <div class="row shipping">
         <div class="col-lg-3 p-2">
             <i class="fa-solid fa-phone-volume"></i>
@@ -117,77 +140,65 @@
     </div>
 
 
-
+    {{-- latest product --}}
     <div class="latest-product">
         <h3 class="text-center mb-5">Latest Product</h3>
 
-        <button style="position: absolute; right: 50px; top: 20px;">
-            <i class="fa-solid fa-arrow-right" style="color: white;"></i>
+        <button style="position: absolute; right: 50px; top: 20px; border: none;">
+            <i class="fa-solid fa-arrow-right" style="color: white; background-color: #2f415d; padding: 8px;"></i>
         </button>
         <div class="row">
             <div class="col-lg-2">
-                <div class="card">
+                <div class="card product-item">
                     <img src="https://m.media-amazon.com/images/I/71kyfjacoBL.jpg" class="product-image" alt="...">
                     <div class="card-body">
                       <h5 class="card-title">Product Name</h5>
-                      <p class="card-text">Short description</p>
                       <p class="card-text">Price</p>
-                      <a href="#" class="btn btn-primary">Buy Now</a>
                     </div>
                   </div>
             </div>
             <div class="col-lg-2">
-                <div class="card">
+                <div class="card product-item">
                     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYUucO-ReCtNTt7aEXznnnCfir-bfyH1XIqQ&usqp=CAU" class="product-image" alt="...">
                     <div class="card-body">
                       <h5 class="card-title">Product Name</h5>
-                      <p class="card-text">Short description</p>
                       <p class="card-text">Price</p>
-                      <a href="#" class="btn btn-primary">Buy Now</a>
                     </div>
                   </div>
             </div>
             <div class="col-lg-2">
-                <div class="card">
-                    <img src="https://fujaelelectronics.com/wp-content/uploads/2023/05/Untitled-design-47-1-600x600.jpg" class="product-image h-25" alt="...">
+                <div class="card product-item">
+                    <img src="https://fujaelelectronics.com/wp-content/uploads/2023/05/Untitled-design-47-1-600x600.jpg" class="product-image" alt="...">
                     <div class="card-body">
                       <h5 class="card-title">Product Name</h5>
-                      <p class="card-text">Short description</p>
                       <p class="card-text">Price</p>
-                      <a href="#" class="btn btn-primary">Buy Now</a>
                     </div>
                   </div>
             </div>
             <div class="col-lg-2">
-                <div class="card">
+                <div class="card product-item">
                     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJdJD8yb4-lkufZlPaXpxRTkIkYN3fqVO8sg&usqp=CAU" class="product-image" alt="...">
                     <div class="card-body">
                       <h5 class="card-title">Product Name</h5>
-                      <p class="card-text">Short description</p>
                       <p class="card-text">Price</p>
-                      <a href="#" class="btn btn-primary">Buy Now</a>
                     </div>
                   </div>
             </div>
             <div class="col-lg-2">
-                <div class="card">
+                <div class="card product-item">
                     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJdJD8yb4-lkufZlPaXpxRTkIkYN3fqVO8sg&usqp=CAU" class="product-image" alt="...">
                     <div class="card-body">
                       <h5 class="card-title">Product Name</h5>
-                      <p class="card-text">Short description</p>
                       <p class="card-text">Price</p>
-                      <a href="#" class="btn btn-primary">Buy Now</a>
                     </div>
                   </div>
             </div>
             <div class="col-lg-2">
-                <div class="card">
+                <div class="card product-item">
                     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJdJD8yb4-lkufZlPaXpxRTkIkYN3fqVO8sg&usqp=CAU" class="product-image" alt="...">
                     <div class="card-body">
                       <h5 class="card-title">Product Name</h5>
-                      <p class="card-text">Short description</p>
                       <p class="card-text">Price</p>
-                      <a href="#" class="btn btn-primary">Buy Now</a>
                     </div>
                   </div>
             </div>
@@ -196,6 +207,73 @@
         </div>
     </div>
 
+    {{-- newsletter --}}
+    <div class="newsletter">
+        <div class="newsletter-text">
+            <h4>SIGN UP OUR NEWSLETTER & GET 10% OFF</h4>
+            <p>Sign up to stay in the loop. Receive updates, access to exclusive deals, and more.</p>
+        </div>
+
+        <div class="newsletter-input">
+            <input type="text" placeholder="Enter Your Email Address">
+            <button>Subscribe</button>
+        </div>
+    </div>
+
+
+    {{-- footer --}}
+    <div class="footer">
+        <div class="footer-logo">
+            <img src="{{asset('/ixony.png')}}" class="logo">
+        </div>
+
+        <div class="footer-info">
+            <h4 class="text-uppercase">Informations</h4>
+            <p>My Account </p>
+            <p>About us</p>
+            <p>Login</p>
+            <p>Privacy policy</p>
+
+        </div>
+
+        <div class="footer-contact">
+            <h4 class="text-uppercase">Contact US</h4>
+            <div>
+
+                <div class="my-2">
+                    <i class="fa-solid fa-location-dot mr-2"></i>
+                    <span>Block C, Banasree</span>
+                </div>
+
+
+                <div class="my-2">
+                    <i class="fa-solid fa-phone mr-2" style="color: #fafcff;"></i>
+                    <span>+88 012 3456 7894</span>
+                </div>
+
+
+                <div class="my-2">
+                    <i class="fa-solid fa-envelope mr-2"></i>
+                    <span>ixony@gmail.com</span>
+
+                </div>
+            </div>
+            <div class="topbar-social">
+                <i class="fa-brands fa-facebook"></i>
+                <i class="fa-brands fa-square-twitter"></i>
+                <i class="fa-brands fa-square-instagram"></i>
+                <i class="fa-brands fa-linkedin"></i>
+            </div>
+        </div>
+    </div>
+
+    <div class="footer-copyright" style="background-color: #2f415d; padding-bottom: 20px; padding-top: 10px;">
+        <p class="text-center text-white">©2023 Ixony. All Rights Reserved.</p>
+    </div>
+
+
+
+    {{-- end footer --}}
 
 
 
@@ -204,48 +282,23 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
 
-    <script src="jquery-3.7.1.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
 
-         <script type="text/javascript">
-            $(document).ready(function(){
-                $('.slider').slick({
 
-                    dots: true,
-                    infinite: false,
-                    speed: 300,
-                    slidesToShow: 4,
-                    slidesToScroll: 4,
-                    responsive: [
-                        {
-                        breakpoint: 1024,
-                        settings: {
-                            slidesToShow: 3,
-                            slidesToScroll: 3,
-                            infinite: true,
-                            dots: true
-                        }
-                        },
-                        {
-                        breakpoint: 600,
-                        settings: {
-                            slidesToShow: 2,
-                            slidesToScroll: 2
-                        }
-                        },
-                        {
-                        breakpoint: 480,
-                        settings: {
-                            slidesToShow: 1,
-                            slidesToScroll: 1
-                        }
-                        }
-                        // You can unslick at a given breakpoint now by adding:
-                        // settings: "unslick"
-                        // instead of a settings object
-                    ]
-                });
-            });
-        </script>
+    <script>
+        $(document).ready(function(){
+          $('.slider').slick({
+            // Slick Slider options go here
+            infinite: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 3000,
+          });
+        });
+      </script>
+
 
 </body>
 </html>
